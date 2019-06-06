1fe7c155656109879171bae7e193e709bc56337a
Vidhyasagar Harihara <vidhyasagar1210@gmail.com>
2019-06-06 12:51:29 -0400

    Modified adding method

--- rangelist.js
+++ rangelist.js
@@ -5,19 +5,31 @@
 class RangeList {
 
   constructor() {
+    // This is going to be a list of lists, with inner lists being of size 2
     this.listOfNumbers = [];
   }
 
   /**
-   * Returns a list of numbers of the form [lower, lower+1, lower+2, ... ,upper-2, upper-1]
+   * Returns +1 if [lower, upper] is strictly to the right of the given range
+   * Returns -1 if [lower, upper] is strictly to the left of the given range
+   * Returns  0 if [lower, upper] is contained/overlapped in the given range
    * @param {Integer} lower, {Integer} upper - Lower and Upper bounds 
    */
-  generateExpandedList(lower, upper) {
-    var expandedList = [];
-    for(var i=lower; i<upper; i++) {
-      expandedList.push(i);
+  getDirection(lower, upper, range) {
+    // This is the case of full consumption, in which case you have to do nothing really
+    // In this case, incoming [lower, upper] is ALREADY FULLY captured by existing range
+    if(lower > range[0] && upper < range[1]) {
+      return 2;
     }
-    return expandedList;
+    else if(lower > range[1]) {
+      return 1;
+    }
+    else if(upper < range[0]) {
+      return -1;
+    }
+    else {
+      return 0;
+    }
   }
   
   /**
@@ -54,38 +66,102 @@ class RangeList {
     var incomingLowerBound = incomingRange[0];
     var incomingUpperBound = incomingRange[1];
 
-    // console.log(`Add function: (${incomingLowerBound}, ${incomingUpperBound})`);
+    console.log(`Add function: (${incomingLowerBound}, ${incomingUpperBound})`);
 
     // Since this means [x,x), which is a contradiction, we ignore this scenario
     if(incomingLowerBound == incomingUpperBound) {
       return;
     }
 
-    var newExpandedList = this.generateExpandedList(incomingLowerBound, incomingUpperBound);
-
     // Trivial base case, when rangeList is initially empty
     if(this.listOfNumbers.length == 0) {
-      this.listOfNumbers = newExpandedList;
+      this.listOfNumbers.push(range);
       return;
     }
     
-    this.listOfNumbers = this.expandList(this.listOfNumbers, newExpandedList);
     
-    // console.log(this.listOfNumbers);
-  }
+    var directionList = [];
+    for(var i=0; i<this.listOfNumbers.length; i++) {
+      directionList.push(this.getDirection(incomingLowerBound, incomingUpperBound, this.listOfNumbers[i]))
+    }
+    
+    // Special case for adding bounds at the beginning/end of list
+    if(directionList[0] == -1) {
+      this.listOfNumbers.unshift(range);
+      return;
+    }
 
-   /**
-   * Expands numberList to include all the numbers present in newList
-   * Then removes duplicates to ensure the range calculations remain accurate
-   * Finally, sorts the list to ensure accuract of range calculations again
-   * @param {Array<number>} numberList - list of integers
-   * @param {Array<number>} newList - list of integers
-   */
-  expandList(numberList, newList) {
-    var updatedList = numberList.concat(newList);
-    updatedList = [...new Set(updatedList)]
-    updatedList = updatedList.sort(function(a, b){return a-b});
-    return updatedList;
+    if(directionList[directionList.length-1] == 1) {
+      this.listOfNumbers.push(range);
+      return;
+    }
+
+    /* 
+    ** Case 1, where incoming bounds form its own unique interval in between
+    ** DirectionList will look like this for this case [1, 1, 1, -1], or [1, -1, -1, -1]
+    */
+    for(var i=0; i<directionList.length-1; i++) {
+      // This means that the new range is between these two ranges, fully internal
+      if(directionList[i] == 1 && directionList[i+1] == -1) {
+        this.listOfNumbers.splice(i+1, 0, range);
+        return;
+      }
+    }
+
+    /*
+    ** Now we will address the case where intervals overlap, and see what can be done with this
+    ** IF intervals overlap, directionList will look something like this [1, 1, 0, 0, -1]
+    ** This signifies that our incoming bounds overlap with 2 existing intervals
+    ** At this point, we are guaranteed to have some 0s, else it would have been satisfied by the case above
+    */
+    var lowerIntersectingBoundIndex = -1;
+    var upperIntersectionBoundIndex = -1;
+    var hasLowerIntersectingBoundBeenFound = false;
+
+    for(var i=0; i<directionList.length; i++) {
+
+      if(!hasLowerIntersectingBoundBeenFound) {
+        if(directionList[i] == 0) {
+          lowerIntersectingBoundIndex = i;
+          hasLowerIntersectingBoundBeenFound = true;
+        }
+      }
+      else {
+        if(directionList[i] != 0) {
+          upperIntersectionBoundIndex = i-1;
+          break;
+        }
+      }
+    }
+
+    // This means there was no '0' in the directionList
+    // This must mean that the inbound interval is ALREADY captured by existing intervals!
+    if(lowerIntersectingBoundIndex == -1) {
+      return;
+    }
+
+    // If we didn't find the upper bound after going through the whole array, then it means that
+    // the upper bound is the last "bounds" itself!
+    if(upperIntersectionBoundIndex == -1) {
+      upperIntersectionBoundIndex = directionList.length - 1;
+    }
+
+    console.log(`lowerIntersectingBoundIndex=${lowerIntersectingBoundIndex} and upperIntersectionBoundIndex=${upperIntersectionBoundIndex}`);
+
+
+    var newLowerBound, newUpperBound;
+    newLowerBound = Math.min(incomingLowerBound, this.listOfNumbers[lowerIntersectingBoundIndex][0]);
+    newUpperBound = Math.max(incomingUpperBound, this.listOfNumbers[upperIntersectionBoundIndex][1]);
+    var newRange = [newLowerBound, newUpperBound];
+
+    // This range should now replace the first entry of the series of elements whose direction is depcited by a 0
+    this.listOfNumbers[lowerIntersectingBoundIndex] = newRange;
+    // Now must delete all elements from lowerIntersectingBoundIndex+1 ... upperIntersectingBoundIndex  
+    for(var i=upperIntersectionBoundIndex; i >= lowerIntersectingBoundIndex+1; i--) {
+      this.listOfNumbers.splice(i, 1);
+    }
+    
+    console.log(this.listOfNumbers);
   }
 
   /**
@@ -99,21 +175,6 @@ class RangeList {
 
     return false;
 
-  }
-
-  /**
-   * Removes all elements of newList that appear in numberList
-   * @param {Array<number>} numberList - list of numbers
-   * @param {Array<number>} newList - list of numbers. Typically, newList.length < numberList.length (not always)
-   */
-  removeIntersectionbetween(numberList, newList) {
-    numberList = numberList.filter(function(element) {
-      return newList.indexOf(element) == -1;
-    });
-
-    // Sort by ascending order, takes care of negative ranges
-    numberList = numberList.sort(function(a, b){return a-b});
-    return numberList;
   }
 
   /**
@@ -132,20 +193,17 @@ class RangeList {
     var incomingLowerBound = incomingRange[0];
     var incomingUpperBound = incomingRange[1];
 
-    // console.log(`Remove function : (${incomingLowerBound}, ${incomingUpperBound})`);
+    console.log(`Remove function : (${incomingLowerBound}, ${incomingUpperBound})`);
 
     // Since this means [x,x), which is a contradiction, we ignore this scenario
     // Also takes care of deleting range in an already empty rangeList
     if(incomingLowerBound == incomingUpperBound || this.listOfNumbers.length == 0) {
       return;
     }
+    
+    // Removal code goes here
 
-    var newExpandedList = this.generateExpandedList(incomingLowerBound, incomingUpperBound);
-
-    // Now we remove every element in this.listOfNumbers that appears in newExpandedList
-    this.listOfNumbers = this.removeIntersectionbetween(this.listOfNumbers, newExpandedList);    
-
-    // console.log(this.listOfNumbers);
+    console.log(this.listOfNumbers);
   }
 
   /**
@@ -157,25 +215,9 @@ class RangeList {
     var upperBound = this.listOfNumbers[this.listOfNumbers.length-1]
     var outputString = '';
 
-    // Special case, if there is only only element left
-    if(this.listOfNumbers.length == 1) {
-      outputString += '[' + lowerBound + ',' + (lowerBound + 1) + ') ';
-    }
-
-    for(var i=1; i<this.listOfNumbers.length; i++) {
-
-      if(this.listOfNumbers[i] - this.listOfNumbers[i-1] != 1) {
-        // Since we are storing [x,y-1], we add +1 to get [x,y)
-        var exclusiveUpperBound = (this.listOfNumbers[i-1] + 1);
-        outputString += '[' + lowerBound + ',' + exclusiveUpperBound + ') ';
-        lowerBound = this.listOfNumbers[i];
-      }
-
-      if(i == this.listOfNumbers.length-1) {
-        // Since we are storing [x,y-1], we add +1 to get [x,y)
-        var exclusiveUpperBound = (upperBound + 1);
-        outputString += '[' + lowerBound + ',' + exclusiveUpperBound + ') ';
-      }
+    for(var i=0; i<this.listOfNumbers.length; i++) {
+      // Each entry is a list of type [x,y]
+      outputString += `[${this.listOfNumbers[i][0]}, ${this.listOfNumbers[i][1]}) `;
     }
 
     console.log(outputString);
